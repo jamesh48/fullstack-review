e8218f37ebb51d05a9504697fa55541f11578234
James Hrivnak <jameshrivnak4@gmail.com>
2021-02-10 22:52:42 -0700

    database connect?

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://JamesH48:CloudlessSky82@cluster0.nkwrm.mongodb.net/fetcher?retryWrites=true&w=majority";
+const connection = "mongodb://JamesH48:CloudlessSky82@cluster0-shard-00-00.nkwrm.mongodb.net:27017,cluster0-shard-00-01.nkwrm.mongodb.net:27017,cluster0-shard-00-02.nkwrm.mongodb.net:27017/fetcher?ssl=true&replicaSet=atlas-tmpem3-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
