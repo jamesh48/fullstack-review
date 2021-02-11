a7ab1af623887722065a0cccc7541599d8825684
James Hrivnak <jameshrivnak4@gmail.com>
2021-02-10 22:58:16 -0700

    Heroku Commit

--- database.js
+++ database.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
-const connection = "mongodb://JamesH48:CloudlessSky82@cluster0-shard-00-00.nkwrm.mongodb.net:27017,cluster0-shard-00-01.nkwrm.mongodb.net:27017,cluster0-shard-00-02.nkwrm.mongodb.net:27017/fetcher?ssl=true&replicaSet=atlas-tmpem3-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const url = process.env.MONGODB_URI;
+
+const connection = url;
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
