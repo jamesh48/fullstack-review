4a17fb50af86d323d863b85680abb228e171bb1f
James Hrivnak <jameshrivnak4@gmail.com>
2021-02-11 22:43:28 -0700

    added a little style to delete collections button

--- client/src/components/dropCollections.jsx
+++ client/src/components/dropCollections.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DropCollections = (props) => {
     {return props.highlighted === true ?
       <div id='drop-collections-button'>
-        <input type='button' onClick={props.dropCollections} value='Drop Collections' />
+        <input id='inner-button' type='button' onClick={props.dropCollections} value='Drop Collections' />
       </div>
       : null
     }
